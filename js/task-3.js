e10cba0dbcd7bb6f2ffbc1260159f27ed31f5eb7
shumskiy <shumskiparents@gmail.com>
2025-11-20 19:02:46 +0100

    hw6

---
+++ js/task-3.js
@@ -0,0 +1,8 @@
+const builder = new StringBuilder(".");
+console.log(builder.getValue()); // "."
+builder.padStart("^");
+console.log(builder.getValue()); // "^."
+builder.padEnd("^");
+console.log(builder.getValue()); // "^.^"
+builder.padBoth("=");
+console.log(builder.getValue()); // "=^.^="
