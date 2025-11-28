58e1eeb280c4c6ae6be7f5e0c2dc7791b8bc526c
shumskiy <shumskiparents@gmail.com>
2025-11-28 12:24:23 +0100

    hw6

--- js/task-1.js
+++ js/task-1.js
@@ -1,6 +1,3 @@
-
-
-
 const customer = {
   username: "Mango",
   balance: 24000,
@@ -8,20 +5,20 @@ const customer = {
   orders: ["Burger", "Pizza", "Salad"],
   // Change code below this line
   getBalance() {
-    return balance;
+    return this.balance;
   },
   getDiscount() {
-    return discount;
+    return this.discount;
   },
   setDiscount(value) {
-    discount = value;
+    this.discount = value;
   },
   getOrders() {
-    return orders;
+    return this.orders;
   },
   addOrder(cost, order) {
-    balance -= cost - cost * discount;
-    orders.push(order);
+    this.balance -= cost - cost * this.discount;
+    this.orders.push(order);
   },
   // Change code above this line
 };
@@ -31,3 +28,5 @@ console.log(customer.getDiscount()); // 0.15
 customer.addOrder(5000, "Steak");
 console.log(customer.getBalance()); // 19750
 console.log(customer.getOrders()); // ["Burger", "Pizza", "Salad", "Steak"]
+
+ 
